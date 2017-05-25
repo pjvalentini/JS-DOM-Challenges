103c7932e9b9be875aabedaa0a9daa6605703885
pjvalentini <pj.valentini@gmail.com>
2017-05-25 17:23:55 -0400

    Updates to code

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # JS-DOM-Challenges
+Created at NYCDA.
+This assignment is to be able to demonstrate understanding of DOM manipulation and events and to build familiarity with JS syntax.
--- keypress.js
+++ keypress.js
@@ -1,8 +1,10 @@
+// Create an HTML page with javascript that listens for a keypress.
+// When the user presses that key, the text of the H1 should show the value
+// of the key they have pressed.
+// Example: If the user presses "J", the text inside the H1 should be "J".
 
-//   document.keypress=function(e){
-//   document.write(.fromCharCode(e.charCode))
-// }
-
+// This Function changes the document HTMl to display on the page.
+// you can press any key in the text field an it will display.
 function keyPress() {
 	document.getElementById().innerHTML = String.fromCharCode(event.charCode);
 }
