734d2e71a6e9378d23c099c7291e5f53643f2d36
pjvalentini <pj.valentini@gmail.com>
2017-05-31 03:09:54 -0400

    Updates to JavaScript Files

--- keypress.js
+++ keypress.js
@@ -3,13 +3,14 @@
 // of the key they have pressed.
 // Example: If the user presses "J", the text inside the H1 should be "J".
 
+
 // Function keyPress(event) listens for the keypress based on the specfic key pressed
 // and displays the key in the document in the h1 element.
 function keyPress(event) {
-	document.getElementById("displayLetter").innerHTML = String.fromCharCode(event.charCode);
+	document.getElementById("displayLetter").innerHTML += String.fromCharCode(event.charCode);
 // console.log(event) allows for the key that you are pressing to be viewed in the console
-// the h1 element will match the key pressed in the console.
-	console.log(event);
+// as well as the h1 element will match the key pressed in the console.
+		console.log(event);
 }
 // This piece of code listens for the keypress.
 document.body.onkeypress = keyPress;
